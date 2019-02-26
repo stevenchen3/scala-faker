420f96705cbf5d6233cd6e3d186debc5efe6eb83
Steven Chen <steven@alphash.io>
2019-02-26 18:11:47 +0800

    feat: include country code and area code to phone

--- src/main/scala/io/alphash/faker/Phone.scala
+++ src/main/scala/io/alphash/faker/Phone.scala
@@ -1,35 +1,94 @@
 package io.alphash.faker
 
+import scala.annotation.tailrec
+import scala.io.Source
 import scala.util.Random
 
-class Phone {
-  // It generates phone numbers of type: "201-886-0269"
-  def phoneNumber: String = {
-    // scalastyle:off
-    val xs = (1 to 10).map(_ ⇒ Random.nextInt(10))
-    s"${xs.slice(0, 3).mkString}-${xs.slice(3, 6).mkString}-${xs.slice(6, 10).mkString}"
-    // scalastyle:on
+sealed case class PhoneFormat(
+  countryCode: String,
+  length: Int,
+  areaCodes: Option[Seq[String]] = None,
+  prefixes: Seq[String],
+  format: String
+)
+sealed case class PhoneModel(formats: Seq[PhoneFormat])
+
+class Phone (models: Map[String, PhoneFormat]) {
+  import Phone._
+
+  private[this] def format(template: String, numbers: Seq[String]): String = {
+    @tailrec
+    def loop(xs: Seq[String], acc: String): String = xs match {
+      case head +: tail ⇒ loop(tail, acc.replaceFirst("X", head))
+      case _ ⇒ acc
+    }
+    loop(numbers, template)
   }
 
-  // It generates phone numbers of type: "(888) 123-4567"
+  private[this] def getPhoneNumber(format: PhoneFormat, enableE164: Boolean = false): String = {
+    val cc  = format.countryCode
+    val rac = areaCodeOption(Some(cc)).getOrElse("")
+    val ac  = if(enableE164) removeHeadingZero(rac) else rac
+    val prefix = getRandomElement[String](format.prefixes).get
+    val tail   = (1 to format.length - prefix.length).map(_ ⇒ Random.nextInt(10)).mkString
+    s"${cc}${ac}${prefix}${tail}"
+  }
+
+  private[this] def randomPhoneFormat = getRandomElement[PhoneFormat](models.map(_._2).toSeq).get
+
+  private[this] def removeHeadingZero(s: String): String =
+    if (s.startsWith("0")) removeHeadingZero(s.drop(1).mkString) else s
+
+  // It generates phone numbers of type: "+65 9012-3456" or "+1 (201) 123-4567"
+  def phoneNumber(countryCode: Option[String] = None): String = countryCode match {
+    case Some(cc) ⇒ models.get(cc).map { phoneFormat ⇒
+      format(phoneFormat.format, getPhoneNumber(phoneFormat).map(_.toString))
+    }.getOrElse(throw new Exception(s"Unsupported country code: $cc"))
+    case None ⇒ format(randomPhoneFormat.format, getPhoneNumber(randomPhoneFormat).map(_.toString))
+  }
+
+  def countryCode: String = getRandomElement[String](models.map(_._1).toSeq).get
+
+  def areaCodeOption(countryCode: Option[String] = None): Option[String] = countryCode match {
+    case Some(cc) ⇒
+      for {
+        pf <- models.get(cc)
+        ac <- pf.areaCodes
+      } yield getRandomElement[String](ac).get
+    case None ⇒ randomPhoneFormat.areaCodes.map(ac ⇒ getRandomElement[String](ac).get)
+  }
+
+  // It generates phone numbers of type: "(800) 123-4567"
   def tollFreePhoneNumber: String = {
     // scalastyle:off
-    val starts = List("777", "888")
+    val starts = List("777", "800", "888")
     val xs = (1 to 7).map(_ ⇒ Random.nextInt(10))
     s"(${starts(Random.nextInt(2))}) ${xs.slice(0, 3).mkString}-${xs.slice(3, 7).mkString}"
     // scalastyle:on
   }
 
   // It generates phone numbers of type: "+71234567891"
-  def e164PhoneNumber: String = {
-    // scalastyle:off
-    val starts = List("7", "8")
-    val xs = (1 to 10).map(_ ⇒ Random.nextInt(10))
-    s"+${starts(Random.nextInt(2))}${xs.mkString}"
-    // scalastyle:on
+  def e164PhoneNumber(countryCode: Option[String]): String = countryCode match {
+    case Some(cc) ⇒ models.get(cc).map { f ⇒
+      s"+${getPhoneNumber(f, true)}"
+    }.getOrElse(throw new Exception(s"Unsupported country code: $cc"))
+    case None ⇒ s"+${getPhoneNumber(randomPhoneFormat, true)}"
   }
 }
 
-object Phone {
-  def apply(): Phone = new Phone()
+object Phone extends Faker {
+  private lazy val model: PhoneModel = {
+    import io.circe.Decoder
+    import io.circe.generic.semiauto.deriveDecoder
+
+    implicit val phoneFormatDecoder: Decoder[PhoneFormat] = deriveDecoder
+    implicit val phoneModelDecoder: Decoder[PhoneModel]   = deriveDecoder
+
+    val s = Source.fromInputStream(getResource("phone", "en.json")).mkString
+    objectFrom[PhoneModel](s)
+  }
+
+  lazy val models: Map[String, PhoneFormat] = model.formats.map(pf ⇒ pf.countryCode -> pf).toMap
+
+  def apply(): Phone = new Phone(models)
 }
--- src/test/scala/io/alphash/faker/PhoneSpec.scala
+++ src/test/scala/io/alphash/faker/PhoneSpec.scala
@@ -3,29 +3,43 @@ package io.alphash.faker
 import org.scalatest.{FlatSpec, Matchers}
 
 class PhoneSpec extends FlatSpec with Matchers {
-  "A phone number" should "have the type of '201-123-4567'" in {
-    val number = Phone().phoneNumber
-    number.size should be(12)
-    val digits = number.split("-")
-    digits.size should be(3)
-    digits(0).size should be(3)
-    digits(1).size should be(3)
-    digits(2).size should be(4)
+  "Country code" should "be one of those in the given list" in {
+    List("1", "65") should contain(Phone().countryCode)
   }
 
-  "A toll free phone number" should "have the type of '(888) 123-4567'" in {
-    val tollfree = Phone().tollFreePhoneNumber
-    tollfree.size should be(14)
-    val digits = tollfree.split(" ")
-    List("(888)", "(777)") should contain(digits(0))
-    digits(1).split("-").size should be(2)
-    digits(1).split("-")(0).size should be(3)
+  "Area code" should "be one of those in the given list" in {
+    List(None, Some("201"), Some("202")) should contain(Phone().areaCodeOption())
   }
 
-  "A E164 phone number" should "have the type of '+71234567891'" in {
-    val number = Phone().e164PhoneNumber
-    number.size should be(12)
-    number.startsWith("+") should be(true)
-    List('7', '8') should contain(number(1))
+  it should "be correct one for a given country" in {
+    Map(Some("65") -> List(None), Some("1") -> List(Some("201"), Some("202"))).foreach { m ⇒
+      m._2 should contain(Phone().areaCodeOption(m._1))
+    }
   }
+
+  //"A phone number" should "have the type of '201-123-4567'" in {
+  //  val number = Phone().phoneNumber
+  //  number.size should be(12)
+  //  val digits = number.split("-")
+  //  digits.size should be(3)
+  //  digits(0).size should be(3)
+  //  digits(1).size should be(3)
+  //  digits(2).size should be(4)
+  //}
+
+  //"A toll free phone number" should "have the type of '(888) 123-4567'" in {
+  //  val tollfree = Phone().tollFreePhoneNumber
+  //  tollfree.size should be(14)
+  //  val digits = tollfree.split(" ")
+  //  List("(888)", "(777)") should contain(digits(0))
+  //  digits(1).split("-").size should be(2)
+  //  digits(1).split("-")(0).size should be(3)
+  //}
+
+  //"A E164 phone number" should "have the type of '+71234567891'" in {
+  //  val number = Phone().e164PhoneNumber
+  //  number.size should be(12)
+  //  number.startsWith("+") should be(true)
+  //  List('7', '8') should contain(number(1))
+  //}
 }
